be43c6ef041a977abe11a87ae7816f6df5e636e7
KiarashYavari <kiarash996@gmail.com>
2022-02-04 11:46:23 +0330

    add asgi chatroom app and its templates

---
+++ chatroom/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+# chat/views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'chatroom/index.html')
+
+
+# chat/views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'chatroom/index.html', {})
+
+def room(request, room_name):
+    return render(request, 'chatroom/room.html', {
+        'room_name': room_name
+    })
