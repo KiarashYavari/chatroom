97deb0888008c75b352c6c5a15dd90d5842003b9
KiarashYavari <kiarash996@gmail.com>
2022-02-04 11:40:48 +0330

    create django channels tutorial project

---
+++ django_channels/asgi.py
@@ -0,0 +1,31 @@
+"""
+ASGI config for django_channels project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chatroom.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channels.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chatroom.routing.websocket_urlpatterns
+        )
+    ),
+    # Just HTTP for now. (We can add other protocols later.)
+})
+
+
+
+
